f49249165b2b98a2bc625ab32719abd57bca6972
chrisbaragar <chrisbaragar@gmail.com>
2015-08-26 08:49:52 -0400

    adds Devise:install and some of the setup steps listed therein

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # CB: Added for Devise support
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_controller.default_url_options = {:host => "localhost", :port => "3000"}
 end
